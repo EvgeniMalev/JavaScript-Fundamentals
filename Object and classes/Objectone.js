fe4e4b35087565d6d1242dbb09c64b3519d6fb85
EvgeniMalev <126523424+EvgeniMalev@users.noreply.github.com>
2024-03-14 22:55:51 +0200

    Create Objectone.js

---
+++ Object and classes/Objectone.js
@@ -0,0 +1,15 @@
+/** Example of an object - car and details and details to it*/
+
+function objectone() {
+
+    let car = {
+        manufacturer : 'Ferreri',
+        model : 'California',
+        year : 2008,
+        price : 231.000,
+        isSportcar : true  
+    };
+  
+    console.log(car.manufacturer);
+}
+objectone();
