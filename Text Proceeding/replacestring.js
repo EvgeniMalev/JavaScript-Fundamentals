0a3315d65d5d1ae80e11c03392c3d33b418c0505
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-03-30 19:53:03 +0200

    Create replacestring.js

---
+++ Text Proceeding/replacestring.js
@@ -0,0 +1,11 @@
+   function replaceRepeatingchars(str) {
+       let result = '';
+	   
+	   for (let idx in str) {
+	      if (str[idx] != str[idx - 1]) {
+		     result += str[idx];
+		   }
+       }
+
+     console.log(result);
+}
