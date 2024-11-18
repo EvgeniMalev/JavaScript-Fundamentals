84b40c0af14fab353c065bf959f6fb0a4b69ffdc
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-11-18 23:10:43 +0200

    Create Concatenatingtextstrings.js

---
+++ Text Proceeding/Concatenatingtextstrings.js
@@ -0,0 +1,11 @@
+var message = "Welcome to my village ";
+var village = "Kozlovets";
+var banger = "!";
+var fullMessage = message + village + banger;
+alert(fullMessage);
+
+var message1 = "The village had ";
+var number = "960";
+var people = " inhabitants";
+var fullMessage1 = message1 + number + people;
+alert(fullMessage1);
